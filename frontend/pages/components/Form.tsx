cee7ad69c794831735c7478363c6ba70f7ca1564
chaklam-silpasuwanchai <chaklam@ait.asia>
2024-01-07 09:03:06 +0700

    add form styling

--- frontend/pages/components/Form.tsx
+++ frontend/pages/components/Form.tsx
@@ -1,5 +1,6 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { InputGroup } from "react-bootstrap";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFileInvoice } from "@fortawesome/free-solid-svg-icons";
@@ -7,25 +8,37 @@ import { faFileInvoice } from "@fortawesome/free-solid-svg-icons";
 export default function FormComponent() {
   const file = (
     <span className="icon-next-to-word">
-      <FontAwesomeIcon icon={faFileInvoice} size="lg" />
+      <FontAwesomeIcon
+        icon={faFileInvoice}
+        size="lg"
+      />
     </span>
   );
   return (
     <div>
       <Form>
-      <div className="form-title">{file} Form title</div>
-        <Form.Group className="mb-3">
-          <Form.Label>Name</Form.Label>
-          <Form.Control placeholder="Enter report name" />
-          <Form.Text>
-            We'll never share your report name with anyone else.
-          </Form.Text>
-        </Form.Group>
+        <div className="form-title">{file} Enter Report Description</div>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Version</Form.Label>
-          <Form.Control placeholder="Enter report version" />
-        </Form.Group>
+        <InputGroup>
+          <InputGroup.Text id="basic-addon1">{file}</InputGroup.Text>
+          <Form.Control
+            type="text"
+            placeholder="Enter report name"
+            aria-label="Enter reporrrttt"
+            aria-describedby="basic-addon1"
+          />
+        </InputGroup>
+
+        <InputGroup>
+          <InputGroup.Text id="basic-addon2">{file}</InputGroup.Text>
+          <Form.Control
+            type="text"
+            placeholder="Enter report name"
+            aria-label="Enter report name"
+            aria-describedby="basic-addon2"
+          />
+        </InputGroup>
+
         <Button variant="primary" type="submit">
           Submit
         </Button>
