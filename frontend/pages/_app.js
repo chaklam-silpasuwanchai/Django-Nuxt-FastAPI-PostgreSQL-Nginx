86c0d5c1f064b7391d0aac9c5d4e07bad1d86b56
akraradets <akraradets@gmail.com>
2024-01-09 18:01:16 +0700

    change frontend to nuxt

--- frontend/pages/_app.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import styles from '../styles/custom.scss';
-
-export default function App({ Component, pageProps }) {
-      return (
-            <Component {...pageProps} />
-      );
-    }
